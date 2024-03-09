f689b91579b420a156ccad71f8d51017178247d4
Spatel903 <patelsohan903@gmail.com>
2024-03-10 02:22:54 +0530

    Comment

--- WebAPI/Controllers/HomeController.cs
+++ WebAPI/Controllers/HomeController.cs
@@ -11,6 +11,8 @@ namespace WebAPI.Controllers
         {
             List<int> numbers = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
 
+
+
             return new JsonResult(numbers);
 
             // return BadRequest();
